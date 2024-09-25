6a934d306c8bcd6a8bc0d6b644dc9d5b42cb2f59
Arthur Hennig <ahennig@student-net-etx-0252.intern.ethz.ch>
2024-09-25 16:57:27 +0200

    Init

--- u01/src/HelloProgrammer.java
+++ u01/src/HelloProgrammer.java
@@ -2,6 +2,12 @@
 public class HelloProgrammer {
     
     public static void main(String[] args) {
-        System.out.println("Hello Programmer");
+        System.out.println("Hello Programmer"); // "Hello Programmer"
+
+        System.out.println(add(10, 25)) // 35
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
