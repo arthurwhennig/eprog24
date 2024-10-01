ed87cde0f1f239747f363ac8340ae63c51a7cdb9
Michael Bernasconi <michael.bernasconi@disneyresearch.com>
2024-10-01 16:40:53 +0200

    Publish u02

---
+++ u02/src/FollerVehler.java
@@ -0,0 +1,9 @@
+public class FollerVehler {
+    public static main(String args) {
+        System.out.println(Hello world);
+        system.out.Pritnln("Gefällt Ihnen dieses Programm"?);
+        System.out.println()
+
+        System.println("Ich habe es selbst geschrieben."; 
+    {
+}
