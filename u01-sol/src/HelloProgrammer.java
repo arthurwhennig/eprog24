4bb3031f6ef5a29cc2175b24bf4999a1f6fd5a06
Michael Bernasconi <michael.bernasconi@disneyresearch.com>
2024-10-01 16:22:38 +0200

    Publish u01-sol

---
+++ u01-sol/src/HelloProgrammer.java
@@ -0,0 +1,10 @@
+/*
+ * Author: Maximiliana Muster
+ * für Einfuehrung in die Programmierung, HS 2024
+ */
+public class HelloProgrammer {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Max!");
+    }
+}
