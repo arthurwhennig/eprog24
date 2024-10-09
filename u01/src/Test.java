29105dbfbc2eafb7ce9350c00794ed2f33b675ce
Arthur Hennig <ahennig@student-net-etx-0176.intern.ethz.ch>
2024-10-09 16:23:45 +0200

    next commit

--- u01/src/Test.java
+++ u01/src/Test.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hey there");
+
+        Scanner console = new Scanner(System.in);
+//        console.nextInt();
+//        console.nextLine();
+        console.close();
+
+        Random zufall = new Random();
+        int a = zufall.nextInt();
+        int b = zufall.nextInt(3, 7);
+        double c = zufall.nextDouble() * 100;
+
+        System.out.println(a + " " + b + " " + c);
+
+
+    }
 }
