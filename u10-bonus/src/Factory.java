4616375cfc56accb6cf9622857a5fcc2299df7d3
Michael Bernasconi <michael.bernasconi@disneyresearch.com>
2024-12-03 16:35:03 +0100

    Publish u10-bonus

---
+++ u10-bonus/src/Factory.java
@@ -0,0 +1,26 @@
+public class Factory {
+	/*
+	 * Zur Erinnerung: Sie duerfern die Anweisungen der Methode computeCost aendern
+	 * aber nicht die Parameterliste oder den Rueckgabewert Wenn es keine public
+	 * static Methode mit diesem Namen und Rueckgabewert gibt, erhalten Sie 0 Punkte
+	 * Sie duerfen in diese Klasse weitere Methoden hinzufuegen
+	 */
+	public static Cost computeCost(Part[] steps) {
+		Cost c = new Cost();
+
+		for (Part p : steps) {
+			p.process(c);
+		}
+
+		return c;
+	}
+
+	public static void main(String[] args) {
+		Cost c = Factory.computeCost(new Part[] { 
+				new EinklappbareRaeder(), 
+				new Fluxkompensator() });
+		
+		System.out.println("Die Kosten betragen sich auf " + c.productionCost + " (MWst " + c.vat + ", Luxus-Steuer "
+				+ c.luxuryTax + ")");
+	}
+}
---
+++ u10-bonus/src/Part.java
@@ -0,0 +1,38 @@
+public class Part {
+
+	public void process(Cost c) {
+		// Ein generischer Part hat keine process() Semantik
+	}
+
+}
+
+
+class Fluegeltueren extends Part {
+	
+}
+
+class Fluxkompensator extends Part {
+	
+}
+
+class Schwebeumwandlung extends Part {
+	
+}
+
+class EinklappbareRaeder extends Part {
+	
+}
+
+class OutatimeKennzeichen extends Part {
+	
+}
+
+//erst relevant in Teil (b)
+class FirstEditionFluxkompensator extends Part {
+	
+}
+
+//erst relevant in Teil (b)
+class VerchromteRaeder extends Part {
+	
+}
