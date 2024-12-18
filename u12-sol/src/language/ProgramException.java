60c84ec70b335fe65007884639c52fecf273f3f0
Michael Bernasconi <michael.bernasconi@disneyresearch.com>
2024-12-18 10:50:24 +0100

    Publish u12-sol

---
+++ u12-sol/src/language/ProgramException.java
@@ -0,0 +1,11 @@
+package language;
+
+/**
+ * Indicates a problem with parsing, interpreting, or compiling a program.
+ */
+public class ProgramException extends Exception {
+    
+    public ProgramException(String message) {
+        super(message);
+    }
+}
